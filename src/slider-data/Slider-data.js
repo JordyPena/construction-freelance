ed89af6f2dc331e1f2f8f879920e2f443faab4d9
Jordy Pena <jordypenamontes@gmail.com>
2021-01-27 08:42:29 -0600

    slider feature || slider added w styling

---
+++ src/slider-data/Slider-data.js
@@ -0,0 +1,45 @@
+import Astoria from '../Images/astoria.jpg'
+import Barnards from '../Images/barnards.jpg'
+import Connors from '../Images/connors.jpg'
+import Day from '../Images/day.jpg'
+import Eckhart from '../Images/eckhart.jpg'
+import Eggerday from '../Images/egger-day.jpg'
+import Egger from '../Images/egger.jpg'
+import Henshaw from '../Images/henshaw.jpg'
+import Matthews from '../Images/matthews.jpg'
+import Nofziger from '../Images/nofziger.jpg'
+
+const data = [
+  {
+    image: Astoria
+  },
+  {
+    image: Barnards
+  },
+  {
+    image:  Connors
+  },
+  {
+    image: Day
+  },
+  {
+    image: Eckhart
+  },
+  {
+    image: Eggerday
+  },
+  {
+    image: Egger
+  },
+  {
+    image: Henshaw
+  },
+  {
+    image: Matthews
+  },
+  {
+    image: Nofziger
+  },
+]
+
+export default data;
