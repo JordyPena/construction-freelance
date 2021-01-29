84d4fdfbdee6172ecdca9d10618dcbe9ea8aeadd
Jordy Pena <jordypenamontes@gmail.com>
2021-01-28 19:19:20 -0600

    services || img and txt add for service on home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Menu from './Menu'
 import Slider from './Slider'
 import Data from '../slider-data/Slider-data'
+import Services from './Services'
+
 function Home() {
   return (
     <>
     <Menu/>
 
     <Slider slides={Data}/>
+    <Services />
     </>
   )
 }
