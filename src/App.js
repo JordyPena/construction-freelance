ed0e700dfb53ecdeae5af898acbc929fc5b61064
Jordy Pena <jordypenamontes@gmail.com>
2021-02-01 18:29:34 -0600

    kit display | features created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Menu from "./components/Menu";
 import Kit from './components/Kit';
 import KitSummary1 from "./components/KitSummary1";
 import KitSummary2 from "./components/KitSummary2";
+import KitSummary3 from "./components/KitSummary3";
+import KitSummary4 from "./components/KitSummary4";
+import KitSummary5 from "./components/KitSummary5";
+import KitSummary6 from "./components/KitSummary6";
 function App() {
   return (
     <>
@@ -46,6 +50,30 @@ function App() {
         path="/kit-summary2"
         render={(props) => {
           return <KitSummary2 />;
+        }}
+      />
+       <Route
+        path="/kit-summary3"
+        render={(props) => {
+          return <KitSummary3 />;
+        }}
+      />
+       <Route
+        path="/kit-summary4"
+        render={(props) => {
+          return <KitSummary4 />;
+        }}
+      />
+       <Route
+        path="/kit-summary5"
+        render={(props) => {
+          return <KitSummary5 />;
+        }}
+      />
+       <Route
+        path="/kit-summary6"
+        render={(props) => {
+          return <KitSummary6 />;
         }}
       />
     </>
